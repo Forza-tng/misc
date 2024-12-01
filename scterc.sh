3c46669da629f6af9d6acdaf32894829981380cf
Forza <forza@noreply.localhost>
2024-12-01 22:31:21 +0100

    Update scterc.sh

--- scterc.sh
+++ scterc.sh
@@ -6,6 +6,15 @@
 # SPDX-License-Identifier: CC0-1.0
 ###
 
+###
+# Description
+#
+# Configure SCTERC timeout for all disks, setting it to 7 seconds for improved
+# error handling. If SCTERC is unsupported, it increases the Linux I/O timeout
+# to 180 seconds. Adjusting timeouts prevents premature I/O failures during
+# extended error recovery, improving reliability.
+###
+
 sct=70      	# 70=7 seconds
 fallback=180	# 180 seconds
 
